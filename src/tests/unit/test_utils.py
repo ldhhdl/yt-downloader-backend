695561acfeea7eca837655ba1e185a98171f942d
a <a>
2025-02-21 21:59:11 +0000

    typing

--- src/tests/unit/test_utils.py
+++ src/tests/unit/test_utils.py
@@ -1,5 +1,6 @@
 import pytest
 from fastapi import HTTPException
+from pytest_mock import MockerFixture
 
 from yt_downloader_backend.routers import video
 
@@ -20,7 +21,7 @@ def test_get_video_code_invalid_url() -> None:
         video._get_video_code(url)
 
 
-def test_check_already_exists(mocker) -> None:
+def test_check_already_exists(mocker: MockerFixture) -> None:
     dynamodb_client = mocker.patch("yt_downloader_backend.clients.get_dynamodb_client")
     dynamodb_client.return_value.query.return_value = {
         "Items": [{"video_code": {"S": VIDEO_CODE}}]
@@ -28,7 +29,7 @@ def test_check_already_exists(mocker) -> None:
     assert video._check_already_exists(TABLE_NAME, VIDEO_CODE)
 
 
-def test_check_already_not_exists(mocker) -> None:
+def test_check_already_not_exists(mocker: MockerFixture) -> None:
     dynamodb_client = mocker.patch("yt_downloader_backend.clients.get_dynamodb_client")
     dynamodb_client.return_value.query.return_value = {"Items": []}
     assert not video._check_already_exists(TABLE_NAME, VIDEO_CODE)
