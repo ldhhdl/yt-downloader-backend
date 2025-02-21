83da45621bd36ff1501064e3b7d202d668cc2739
a <a>
2025-02-21 20:31:09 +0000

    rotating out secrets

--- src/yt_downloader_backend/config.py
+++ src/yt_downloader_backend/config.py
@@ -7,12 +7,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    bucket_name: str = "video-downloads"
-    queue_url: str = "blah"
-    table_name: str = "video-downloads"
-    aws_region: str = "us-east-1"
-    aws_access_key_id: Secret[str] = "AKIA4IM3HDCO5O2KLTXS"
-    aws_secret_access_key: Secret[str] = "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"
+    bucket_name: str
+    queue_url: str
+    table_name: str
+    aws_region: str
+    aws_access_key_id: Secret[str]
+    aws_secret_access_key: Secret[str]
 
 
 @functools.lru_cache
